b68a9bc25598c1ed15b585e40296d4bb2a0a21ac
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2023-10-14 14:08:12 -0400

    tryexcept.py

---
+++ tryexcept.py
@@ -0,0 +1,40 @@
+try: 
+    #k = 5//0 # raises divide by zero exception. 
+    #print(k) 
+    h = "i" + 3
+    print(h)
+    
+# handles zerodivision exception     
+except ZeroDivisionError:    
+    print("Can't divide by zero") 
+except TypeError:
+    print("Invalid Types")
+        
+finally: 
+    # this block is always executed  
+    # regardless of exception generation. 
+    print('This is always executed')  
+
+#output: Invalid Types
+#        This is always executed
+
+try: 
+    k = 5//0 # raises divide by zero exception. 
+    print(k) 
+    #h = "i" + 3
+    #print(h)
+    
+# handles zerodivision exception     
+except ZeroDivisionError:    
+    print("Can't divide by zero") 
+except TypeError:
+    print("Invalid Types")
+        
+finally: 
+    # this block is always executed  
+    # regardless of exception generation. 
+    print('This is always executed')  
+
+#output: Can't divide by zero
+#        This is always executed 
+
